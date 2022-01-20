e4d3231d0868e42e37748b36be525f76252730f6
Nahom Assefa <nzenebe@gmail.com>
2022-01-20 15:32:22 -0600

    new push

--- controllers/api/html-routes.js
+++ controllers/api/html-routes.js
@@ -22,96 +22,96 @@ const { User, Post, Comment, Vote, Question } = require("../../models");
 //     }
 // })
 
-// router.get("/", async (req, res) => {
-//   try {
-//     const response = await Post.findAll({
-//         where: {
-//             topic = req.body.post_topic
-//         },
-//       attributes: [
-//         "id",
-//         "title",
-//         "post_text",
-//         "created_at",
-//         [
-//           sequelize.literal(
-//             "SELECT COUNT(*) FROM vote WHERE post_id = vote.post.id"
-//           ),
-//         ],
-//       ],
-//       order: [["created_at", "DESC"]],
-//       include: [
-//         {
-//           model: User,
-//           attributes: ["username"],
-//         },
-//         {
-//           model: Question,
-//           attributes: ["q_text"]
-//         },
-//         {
-//           model: Comment,
-//           attributes: [
-//             "id",
-//             "comment_text",
-//             "user_id",
-//             "post_id",
-//             "created_at",
-//           ],
-//           include: {
-//             model: User,
-//             attributes: ["username"],
-//           },
-//         },
-//       ],
-//     });
-//     res.status(200).json(response);
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
+router.get("/", async (req, res) => {
+  try {
+    const response = await Post.findAll({
+        where: {
+            topic = req.body.post_topic
+        },
+      attributes: [
+        "id",
+        "title",
+        "post_text",
+        "created_at",
+        [
+          sequelize.literal(
+            "SELECT COUNT(*) FROM vote WHERE post_id = vote.post.id"
+          ),
+        ],
+      ],
+      order: [["created_at", "DESC"]],
+      include: [
+        {
+          model: User,
+          attributes: ["username"],
+        },
+        {
+          model: Question,
+          attributes: ["q_text"]
+        },
+        {
+          model: Comment,
+          attributes: [
+            "id",
+            "comment_text",
+            "user_id",
+            "post_id",
+            "created_at",
+          ],
+          include: {
+            model: User,
+            attributes: ["username"],
+          },
+        },
+      ],
+    });
+    res.status(200).json(response);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
-// router.get("/:id", async (req, res) => {
-//     try {
-//         const response = await Post.findOne({
-//           where: {
-//             topic: req.body.post_topic,
-//             id: req.params.id,
-//           },
-//           attributes: ["id", "title", "post_topic", "post_text", "created_at"],
-//           include: [
-//             {
-//               model: User,
-//               attribute: ["username"],
-//             },
-//             {
-//               model: Question,
-//               attribute: ["q_text"],
-//             },
-//             {
-//               model: Comment,
-//               attributes: [
-//                 "id",
-//                 "comment_text",
-//                 "user_id",
-//                 "post_id",
-//                 "created_at",
-//               ],
-//               include: {
-//                 model: User,
-//                 attribute: ["username"],
-//               },
-//             },
-//           ],
-//         });
-//         if (!response) {
-//           res.status(400).json(err);
-//         }
-//         res.status(200).json(response);
-//       } catch (err) {
-//         res.status(500).json(err);
-//       }
-// })
+router.get("/:id", async (req, res) => {
+    try {
+        const response = await Post.findOne({
+          where: {
+            topic: req.body.post_topic,
+            id: req.params.id,
+          },
+          attributes: ["id", "title", "post_topic", "post_text", "created_at"],
+          include: [
+            {
+              model: User,
+              attribute: ["username"],
+            },
+            {
+              model: Question,
+              attribute: ["q_text"],
+            },
+            {
+              model: Comment,
+              attributes: [
+                "id",
+                "comment_text",
+                "user_id",
+                "post_id",
+                "created_at",
+              ],
+              include: {
+                model: User,
+                attribute: ["username"],
+              },
+            },
+          ],
+        });
+        if (!response) {
+          res.status(400).json(err);
+        }
+        res.status(200).json(response);
+      } catch (err) {
+        res.status(500).json(err);
+      }
+})
 
 
 
--- models/Question.js
+++ models/Question.js
@@ -1,40 +1,49 @@
-// const { Model, DataTypes } = require("sequelize");
-// const sequelize = require("../config/connection");
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
 
-// class Question extends Model {}
+class Question extends Model {}
 
-// Question.init(
-//   {
-//     id: {
-//       type: DataTypes.INTEGER,
-//       primaryKey: true,
-//       autoIncrement: true,
-//       allowNull: false,
-//     },
-//     q_text: {
-//       type: DataTypes.TEXT,
-//       allowNull: false,
-//       unique: true,
-//     },
-//     user_id: {
-//       type: DataTypes.INTEGER,
-//       references: {
-//         model: "user",
-//         key: "id",
-//       },
-//     },
-//     post_id: {
-//       model: "post",
-//       key: "id",
-//     },
-//   },
-//   {
-//     sequelize,
-//     timestamps: false,
-//     freezeTableName: true,
-//     underscored: true,
-//     modelName: "vote",
-//   }
-// );
+Question.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    q_text: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      unique: true,
+    },
+    q_possible: {
+      type: DataTypes.ARRAY,
+      allowNull: false,
+    },
+    q_answer: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      unique: true,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+    post_id: {
+      model: "post",
+      key: "id",
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "vote",
+  }
+);
 
-// module.exports = Question;
+module.exports = Question;
