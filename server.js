77496f12160471c2c4aeed9e7354044f5d66dae4
Nahom Assefa <nzenebe@gmail.com>
2022-01-20 16:47:02 -0600

    server adjustment

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const helpers = require("./utils/helpers");
 const exphbs = require("express-handlebars");
 const hbs = exphbs.create({ helpers });
 const app = express();
-const PORT = process.env.PORT || 3009;
+const PORT = process.env.PORT || 3001;
 
 // Session requirements
 const sess = {
