1d7739da37bae5d6df638e9bcf61336de77552ff
joshgit <jschermann91@gmail.com>
2022-01-21 08:21:55 -0600

    update port

--- utils/quiz.js
+++ utils/quiz.js
@@ -3,6 +3,7 @@ let countdown = $("#countdown");
 let timeLeft = 300;
 let startPageArray = [];
 let currentQuestion = 0;
+
 const questionArray = [
     {
       title: "What does JSON stand for?",
@@ -89,20 +90,20 @@ const questionArray = [
   ];
 
 //https://michael-karen.medium.com/how-to-save-high-scores-in-local-storage-7860baca9d68
-const NO_OF_HIGH_SCORES = 10;
-const HIGH_SCORES = "highScores";
-const highScoreString = localStorage.getItem(HIGH_SCORES);
-const highScores = JSON.parse(highScoreString) ?? [];
+// const NO_OF_HIGH_SCORES = 10;
+// const HIGH_SCORES = "highScores";
+// const highScoreString = localStorage.getItem(HIGH_SCORES);
+// const highScores = JSON.parse(highScoreString) ?? [];
 
-function checkHighScore(score) {
-  const highScores = JSON.parse(localStorage.getItem(HIGH_SCORES)) ?? [];
-  const lowestScore = highScores[NO_OF_HIGH_SCORES - 1]?.score ?? 0;
+// function checkHighScore(score) {
+//   const highScores = JSON.parse(localStorage.getItem(HIGH_SCORES)) ?? [];
+//   const lowestScore = highScores[NO_OF_HIGH_SCORES - 1]?.score ?? 0;
 
-  if (score > lowestScore) {
-    saveHighScore(score, highScores);
-    showHighScores();
-  }
-}
+//   if (score > lowestScore) {
+//     saveHighScore(score, highScores);
+//     showHighScores();
+//   }
+// }
 
 function saveHighScore(score, highScores) {
   const name = prompt("You got a highscore! Enter name:");
@@ -110,58 +111,58 @@ function saveHighScore(score, highScores) {
   // timeLeft = 0;
 
   // 1. Add to list
-  highScores.push(newScore);
+  // highScores.push(newScore);
 
-  // 2. Sort the list
-  highScores.sort((a, b) => b.score - a.score);
+  // // 2. Sort the list
+  // highScores.sort((a, b) => b.score - a.score);
 
-  // 3. Select new list
-  highScores.splice(NO_OF_HIGH_SCORES);
+  // // 3. Select new list
+  // highScores.splice(NO_OF_HIGH_SCORES);
 
-  // 4. Save to local storage
-  localStorage.setItem(HIGH_SCORES, JSON.stringify(highScores));
+  // // 4. Save to local storage
+  // localStorage.setItem(HIGH_SCORES, JSON.stringify(highScores));
 }
 
-let showHighScores = function () {
-  // Clear screen
-  $("main").html("");
+// let showHighScores = function () {
+//   // Clear screen
+//   $("main").html("");
 
-  // Display end game screen
-  $("main").append(
-    "<h2 class='col-8 offset-2 col-sm-6 offset-sm-3 col-md-6 offset-md-3 col-lg-4 offset-lg-4 text-white text-center'>HIGH SCORES<h2>"
-  );
-  $("main").append(
-    "<ol id='highScores' class='col-8 offset-2 col-sm-6 offset-sm-3 col-md-6 offset-md-3 col-lg-4 offset-lg-4 text-white text-center'></ol>"
-  );
+//   // Display end game screen
+//   $("main").append(
+//     "<h2 class='col-8 offset-2 col-sm-6 offset-sm-3 col-md-6 offset-md-3 col-lg-4 offset-lg-4 text-white text-center'>HIGH SCORES<h2>"
+//   );
+//   $("main").append(
+//     "<ol id='highScores' class='col-8 offset-2 col-sm-6 offset-sm-3 col-md-6 offset-md-3 col-lg-4 offset-lg-4 text-white text-center'></ol>"
+//   );
 
-  //https://michael-karen.medium.com/how-to-save-high-scores-in-local-storage-7860baca9d68
-  const highScores = JSON.parse(localStorage.getItem(HIGH_SCORES)) ?? [];
-  const highScoreList = document.getElementById(HIGH_SCORES);
+//   //https://michael-karen.medium.com/how-to-save-high-scores-in-local-storage-7860baca9d68
+//   const highScores = JSON.parse(localStorage.getItem(HIGH_SCORES)) ?? [];
+//   const highScoreList = document.getElementById(HIGH_SCORES);
 
-  highScoreList.innerHTML = highScores
-    .map((score) => `<li>${score.score}............${score.name}`)
-    .join("");
+//   highScoreList.innerHTML = highScores
+//     .map((score) => `<li>${score.score}............${score.name}`)
+//     .join("");
 
-  // Create restart button
-  $("main").append(
-    "<button id='restart' class='col-8 offset-2 col-sm-6 offset-sm-3 col-md-6 offset-md-3 col-lg-4 offset-lg-4 text-white text-center bg-secondary'>Go Back</button>"
-  );
-  // Add event listener to restart/go back button
-  $("#restart").on("click", function () {
-    restartGame();
-  });
+//   // Create restart button
+//   $("main").append(
+//     "<button id='restart' class='col-8 offset-2 col-sm-6 offset-sm-3 col-md-6 offset-md-3 col-lg-4 offset-lg-4 text-white text-center bg-secondary'>Go Back</button>"
+//   );
+//   // Add event listener to restart/go back button
+//   $("#restart").on("click", function () {
+//     restartGame();
+//   });
 
-  // Create clear highscores button
-  $("main").append(
-    "<button id='clearScores' class='col-8 offset-2 col-sm-6 offset-sm-3 col-md-6 offset-md-3 col-lg-4 offset-lg-4 text-white text-center bg-secondary mt-2'>Clear High Scores</button>"
-  );
-  // Add event listener to restart/go back button
-  $("#clearScores").on("click", function () {
-    alert("Clearing Scores");
-    window.localStorage.removeItem("highScores");
-    window.location.reload();
-  });
-};
+//   // Create clear highscores button
+//   $("main").append(
+//     "<button id='clearScores' class='col-8 offset-2 col-sm-6 offset-sm-3 col-md-6 offset-md-3 col-lg-4 offset-lg-4 text-white text-center bg-secondary mt-2'>Clear High Scores</button>"
+//   );
+//   // Add event listener to restart/go back button
+//   $("#clearScores").on("click", function () {
+//     alert("Clearing Scores");
+//     window.localStorage.removeItem("highScores");
+//     window.location.reload();
+//   });
+// };
 
 const endGame = function () {
   checkHighScore(timeLeft);
@@ -180,10 +181,10 @@ $(document).ready(() => {
     startGame();
   });
 
-  $("#highscoreChart").on("click", function () {
-    showHighScores();
-    timeLeft = 0;
-  });
+  // $("#highscoreChart").on("click", function () {
+  //   showHighScores();
+  //   timeLeft = 0;
+  // });
 });
 
 const buildQuestionTemplate = () => {
