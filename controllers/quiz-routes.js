1211d15a98b8fffc03c1b37db536b78940e884a5
joshgit <jschermann91@gmail.com>
2022-01-22 11:46:30 -0600

    quiz routes added, index updated

---
+++ controllers/quiz-routes.js
@@ -0,0 +1,18 @@
+const router = require("express").Router();
+const sequelize = require("../config/connection");
+const { Post, User, Comment, Vote} = require("../models");
+
+// Display JS quiz at quiz/js
+router.get("/js", (req, res) => {
+
+try {
+    res.render("jsquiz", {
+    // loggedIn: req.session.loggedIn,
+});
+} catch (error) {
+    console.log(error);
+    res.status(500).json(err);
+}
+});
+
+module.exports = router;
