e94021c74574ee66fc60dbc4a80086668d1e9079
Nahom Assefa <nzenebe@gmail.com>
2022-01-22 16:08:55 -0600

    add-resource functionality

--- public/javascript/add-resource.js
+++ public/javascript/add-resource.js
@@ -1,12 +1,13 @@
 async function addResource(event) {
   event.preventDefault();
 
-  const title = document.querySelector('input[name="title"]').value;
-  const topic = document.querySelector('input[name="topic"]').value;
-  const text = document.querySelector('textarea[name="text"]').value;
-  const user_id = document.querySelector('input[name="user_id"]').value;
-//   const user_id = 2;
+  const title = document.querySelector('input[name="title"]').value.trim();
+  const topic = document.querySelector('input[name="topic"]').value.trim();
+  const text = document.querySelector('textarea[name="text"]').value.trim();
+  const user_id = document.querySelector('input[name="user_id"]').value.trim();
+  console.log('line 8', title, topic, text, user_id);
 
+  if (title && topic && text && user_id) {
   const response = await fetch("/api/posts", {
     method: "Post",
     body: JSON.stringify({
@@ -15,7 +16,7 @@ async function addResource(event) {
       text,
       user_id,
     }),
-    Headers: { "Content-Type": "application/json" },
+    headers: { "Content-Type": "application/json" },
   });
 
   if (response.ok) {
@@ -24,6 +25,12 @@ async function addResource(event) {
     response.statusText;
   }
 
+if (response.ok) {
+    document.location.replace('/profile')
+} else {
+    alert(response.statusText);
+}
+}
 }
 
 document.querySelector(".new-post-form").addEventListener("submit", addResource);
