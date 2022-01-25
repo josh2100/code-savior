f41cdf11352f7cba0f1db1af3adfbed367888b5a
joshgit <jschermann91@gmail.com>
2022-01-24 20:28:07 -0600

    vote test

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 const User = require("./User")
 const Post = require("./Post");
-const Vote = require("./Vote");
+const Vote = require("./TestVote");
 const Comment = require("./Comment");
 const Question = require("./Question");
 
