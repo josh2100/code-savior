2bc79c424a6872b6e560939f0fbe1ddfbe9edcaa
joshgit <jschermann91@gmail.com>
2022-01-25 11:59:46 -0600

    quiz errors fixed

--- public/javascript/quiz.js
+++ public/javascript/quiz.js
@@ -1,13 +1,7 @@
-//global variables
-// let countdown = document.querySelector("#countdown");
-let timeLeft = 300;
-let startPageArray = [];
 let currentQuestion = 0;
-let questionArray = [
-];
+let questionArray = [];
 
 const fetchQuiz = async () => {
-  // async function fetchQuiz(event) {
   const response = await fetch("/api/js", {
     method: "GET",
     headers: { "Content-Type": "application/json" },
@@ -16,27 +10,26 @@ const fetchQuiz = async () => {
   if (response.ok) {
     const data = await response.json();
 
-    data.forEach(({ title, answer1, answer2, answer3, answer4, correctAnswer}) => {
-         questionArray.push( {title, answer1, answer2, answer3, answer4, correctAnswer} );
-    })
-   
-    console.log(questionArray);
+    data.forEach(
+      ({ title, answer1, answer2, answer3, answer4, correctAnswer }) => {
+        questionArray.push({
+          title,
+          answer1,
+          answer2,
+          answer3,
+          answer4,
+          correctAnswer,
+        });
+      }
+    );
+
   } else {
     response.statusText;
   }
 };
 
-// Captures HTML at start so it can be replaced when game ends
-const recordStartPage = () => {
-  startPageArray = $("main").html();
-};
-
 // Wait for document to render
 window.addEventListener("DOMContentLoaded", async (event) => {
-  console.log("DOM fully loaded and parsed");
-
-  recordStartPage();
-
   // Fetch questions
   fetchQuiz();
 
@@ -57,16 +50,14 @@ function checkHighScore(score) {
 }
 
 function saveHighScore(score, highScores) {
-    /// Change to Modal
+  /// Change to Modal
   const accuracyModal = alert("Your accuracy was:");
   restartGame();
-}
+};
 
 let showHighScores = function () {};
-/////
 
 const endGame = function () {
-  console.log("game end");
   checkHighScore(timeLeft);
 };
 
@@ -74,8 +65,6 @@ const buildQuestionTemplate = () => {
   // Check if there are any more questions
   const checkIfGameEnd = function () {
     if (currentQuestion >= questionArray.length) {
-      endGame();
-    } else if (timeLeft <= 0) {
       endGame();
     } else {
       buildQuestionTemplate();
@@ -97,23 +86,13 @@ const buildQuestionTemplate = () => {
   $("main").html("");
 
   // Add question
-  $("main").append(
-    "<br><br><p id='title' class='col-8 offset-2 col-sm-6 offset-sm-3 col-md-6 offset-md-3 col-lg-4 offset-lg-4 text-white'></p><br><br>"
-  );
+  $("main").append("<br><br><p id='title' class=''></p><br><br>");
 
   /// Add answer buttons
-  $("main").append(
-    "<button id='answer1' class='col-6 offset-3 col-md-6 offset-md-3 text-white bg-secondary mt-2 p-2'>ANSWER1</button>"
-  );
-  $("main").append(
-    "<button id='answer2' class='col-6 offset-3 col-md-6 offset-md-3 text-white bg-secondary mt-2 p-2'>ANSWER2</button>"
-  );
-  $("main").append(
-    "<button id='answer3' class='col-6 offset-3 col-md-6 offset-md-3 text-white bg-secondary mt-2 p-2'>ANSWER3</button>"
-  );
-  $("main").append(
-    "<button id='answer4' class='col-6 offset-3 col-md-6 offset-md-3 text-white bg-secondary mt-2 p-2'>ANSWER4</button>"
-  );
+  $("main").append("<button id='answer1' class=''>ANSWER1</button>");
+  $("main").append("<button id='answer2' class=''>ANSWER2</button>");
+  $("main").append("<button id='answer3' class=''>ANSWER3</button>");
+  $("main").append("<button id='answer4' class=''>ANSWER4</button>");
 
   // Fill in question title
   $("#title").text(questionArray[currentQuestion].title);
@@ -161,39 +140,14 @@ let startGame = function () {
   // Clear screen
   $("main").html("");
 
-  console.log("game started");
-
   // Reset variables
   timeLeft = 300;
   currentQuestion = 0;
-
-  //https://www.freecodecamp.org/news/javascript-timers-everything-you-need-to-know-5f31eaa37162/
-  let clock = () => {
-    if (timeLeft > 1) {
-      timeLeft--;
-    } else {
-      showHighScores(timeLeft);
-      endGame();
-      clearInterval(increment);
-    }
-
-    if (!questionArray[currentQuestion]) {
-      //   showHighScores(timeLeft);
-      clearInterval(increment);
-    }
-  };
-  // Start timer
-  let increment = setInterval(clock, 1000);
 
   // Build question template
   buildQuestionTemplate();
 };
 
 let restartGame = function () {
-  // Loads saved HTML
-  $("main").html(startPageArray);
-  // Re-Apply Event Listener
-  $("#start-btn").on("click", function () {
-    startGame();
-  });
+  document.location.reload();
 };
